36734f85ab82f38c9ba446d9ba24da0d9368f483
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-19 15:57:47 +0530

    solution

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function convertToRoman(num) {
     };
 
   //your code here
+  
   let result = "";
   let i = 0;
   
@@ -24,7 +25,6 @@ function convertToRoman(num) {
   
   return result;
 }
-
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
